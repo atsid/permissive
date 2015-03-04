a29dad53ea46ec8c02b18821a1f8ebcd77257d6d
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-04 15:09:32 -0800

    #18 - Middleware for retrieving github users

--- app/server/services/github/users.js
+++ app/server/services/github/users.js
@@ -3,11 +3,17 @@
 var github = require('../../lib/github');
 
 module.exports = {
-    get: function () {
+    getUsers: function () {
         return github.getMembers({
             user: github.config.username,
             org: github.config.org,
             per_page: 100
+        });
+    },
+
+    getUser: function (username) {
+        return github.getFrom({
+            user: username
         });
     }
 };

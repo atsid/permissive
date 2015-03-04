e5a8752e77717ed1ed7b7596c8278cf01351050e
Chris Trevino <chris.trevino@atsid.com>
2015-03-04 14:29:48 -0800

    Adding clustering to the dev-server

--- dev-server.js
+++ dev-server.js
@@ -1,4 +1,28 @@
 'use strict';
 
-var permissive = require('./app/main');
-permissive.start();
+var cluster = require('cluster'),
+    workerLimit = process.env['WORKER_LIMIT'],
+    clusteringEnabled = process.env['ENABLE_CLUSTERING'],
+    startApplication = function () {
+        var permissive = require('./app/main');
+        permissive.start();
+    };
+
+if (clusteringEnabled && cluster.isMaster) {
+    var cpuCount = require('os').cpus().length,
+        workerCount = workerLimit ? workerLimit : cpuCount;
+
+    console.log("Spawning " + workerCount + " workers");
+    for (var i = 0; i < workerCount; i += 1) {
+        console.log("spawning worker");
+        cluster.fork();
+    }
+    cluster.on('disconnect', function (worker) {
+        console.error('**worker disconnected**', worker);
+        cluster.fork();
+    });
+} else {
+    startApplication();
+}
+
+

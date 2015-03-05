f98938453fe8e9f577ecca44d67c5ce929b34ef7
Chris Trevino <chris.trevino@atsid.com>
2015-03-04 16:50:17 -0800

    Adding app.js to static analysis

--- app.js
+++ app.js
@@ -2,24 +2,27 @@
 
 var cluster = require('cluster'),
     cpuCount = require('os').cpus().length,
-    workerLimit = process.env.WORKER_LIMIT,
     clusteringEnabled = process.env.ENABLE_CLUSTERING,
-    workerCount = workerLimit ? workerLimit : cpuCount,
     i,
+    startMaster = function () {
+        var workerLimit = process.env.WORKER_LIMIT,
+            workerCount = workerLimit ? workerLimit : cpuCount;
+        console.log("Spawning " + workerCount + " workers");
+        for (i = 0; i < workerCount; i += 1) {
+            cluster.fork();
+        }
+        cluster.on('exit', function (worker, code, signal) {
+            console.log('worker %d died (%s). restarting...', worker.process.pid, signal || code);
+            cluster.fork();
+        });
+    },
     startApplication = function () {
         var permissive = require('./app/main');
         permissive.start();
     };
 
 if (clusteringEnabled && cluster.isMaster) {
-    console.log("Spawning " + workerCount + " workers");
-    for (i = 0; i < workerCount; i += 1) {
-        cluster.fork();
-    }
-    cluster.on('exit', function (worker, code, signal) {
-        console.log('worker %d died (%s). restarting...', worker.process.pid, signal || code);
-        cluster.fork();
-    });
+    startMaster();
 } else {
     startApplication();
 }

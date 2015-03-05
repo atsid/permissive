aed3a12bca18e7bc44ec65ac900a950d04f4eb8e
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-05 14:43:48 -0800

    #19 - middleware to read repos

--- app/server/middleware/repos.js
+++ app/server/middleware/repos.js
@@ -1,19 +1,29 @@
 'use strict';
 
+var repoService = require('../components/repositories/repos');
+
 module.exports = {
 
     listRepos: (req, res, next) => {
         console.log('listing repos [' + req.path + ']');
         console.log('query:' + JSON.stringify(req.query, null, 2));
 
-        req.entity = [{
-            id: 1,
-            name: 'stub-repo-1'
-        }, {
-            id: 2,
-            name: 'stub-repo-2'
-        }];
-        next();
+        repoService.getRepos().then((repos) => {
+
+            req.entity = repos.map((repo) => {
+                return {
+                    'id': repo.id,
+                    'name': repo.name,
+                    'description': repo.description,
+                    'public': !repo.private
+                };
+            });
+
+            next();
+
+        }).catch((err) => {
+            next(err);
+        });
     },
 
     listReposPermission: (req, res, next) => {
@@ -56,10 +66,23 @@ module.exports = {
         console.log('getting repo [' + req.path + ']');
         console.log('params:' + JSON.stringify(req.params, null, 2));
 
-        req.entity = {
-            id: req.params.id,
-            name: 'stub-repo-' + req.params.id
-        };
-        next();
+        repoService.getRepos().then((repos) => {
+
+            let repo = repos.find((r) => {
+                return r.id === parseInt(req.params.id, 10);
+            });
+
+            req.entity = {
+                'id': repo.id,
+                'name': repo.name,
+                'description': repo.description,
+                'public': !repo.private
+            };
+
+            next();
+
+        }).catch((err) => {
+            next(err);
+        });
     }
 };

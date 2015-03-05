f9d10e95e17de0ae1dfa528f43e1c0f45a107f9c
Chris Trevino <chris.trevino@atsid.com>
2015-03-04 23:36:31 -0800

    #27 - Build Updates * Adding nodemon for app-server lifecycle management in development. * Adding tasks for starting an app-server, executing integration tests, and terminating the app-server. * Removing gulp-changed and gulp-rimraf, as we're not using those kinds of tasks at the moment.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,10 @@
 'use strict';
-var gulp         = require('gulp'),
-    jshint       = require('gulp-jshint'),
-    jscs         = require('gulp-jscs'),
-    gutil        = require('gulp-util'),
-    mocha        = require('gulp-mocha'),
-    changed      = require('gulp-changed'),
-    rimraf       = require('gulp-rimraf'),
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    mocha = require('gulp-mocha'),
+    nodemon = require('gulp-nodemon'),
+    gutil = require('gulp-util'),
 
     /**
      * Build Constants
@@ -13,44 +12,87 @@ var gulp         = require('gulp'),
     APP_SRC = 'app/**/*.js',
     ALL_SRC = [APP_SRC, '*.js'],
     SERVER_TEST_SRC = 'app/server/test/**/Test*.js',
-    SERVER_IT_SRC = 'app/server/it/**/Test*.js';
+    SERVER_IT_SRC = 'app/server/it/**/Test*.js',
+
+    devServer = null;
 
 /**
- * Javascript Linting
+ * Static Analysis Tasks
  */
 gulp.task('lint', () => {
     return gulp.src(ALL_SRC)
-       .pipe(jshint({lookup: true}))
-       .pipe(jshint.reporter('default'));
+        .pipe(jshint({lookup: true}))
+        .pipe(jshint.reporter('default'));
 });
 
-/**
- * Javascript Code Style Checking
- */
 gulp.task('jscs', () => {
     return gulp.src(ALL_SRC)
         .pipe(jscs());
 });
 
 /**
- * Unit Testing
+ * Testing Tasks
  */
-gulp.task('server-test', function () {
+gulp.task('server-unit-test', () => {
     return gulp.src(SERVER_TEST_SRC)
         .pipe(mocha({reporter: 'spec'}));
 });
 
-/**
- * Integration Testing
- */
-gulp.task('server-it', function () {
+gulp.task('server-integration-tests', ['server'], () => {
     return gulp.src(SERVER_IT_SRC)
         .pipe(mocha({reporter: 'spec'}));
 });
 
+gulp.task('execute-server-integration-tests', [
+    'server',
+    'server-integration-tests',
+    'halt-server'
+]);
+
+/**
+ * App-Server Startup (for test)
+ */
+gulp.task('server', () => {
+    return new Promise((resolve, reject) => {
+        devServer = nodemon({
+            script: 'app.js',
+            ext: 'js',
+            watch: ['app/server/**/*.js'],
+            env: {
+                'NODE_ENV': 'development'
+            },
+            nodeArgs: ['--harmony'],
+            stdout: false
+        });
+
+        devServer
+        .on('exit', () => {
+            gutil.log("Process exiting");
+        })
+        .on('change', ['static-checking'])
+        .on('restart', () => {
+            gutil.log('**** Server Restarted ****');
+        })
+        .on('stdout', (data) => {
+            let output = data.toString();
+            if (output.indexOf("Express server listening on port") !== -1) {
+                gutil.log("**** Server Ready ****");
+                resolve(devServer);
+            }
+        });
+    });
+});
+
+gulp.task('halt-server', ['server-integration-tests'], () => {
+    devServer.emit('quit');
+});
+
+gulp.task('static-checking', [
+    'lint',
+    'jscs'
+]);
 
 gulp.task('default', [
-    'lint',
-    'jscs',
-    'server-test'
+    'static-checking',
+    'server-unit-test'
 ]);

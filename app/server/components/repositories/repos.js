13b02bca7aa1d69b8cdb49b0480a2a35e5040b98
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-06 10:00:54 -0800

    #19 - removing some un needed code

--- app/server/components/repositories/repos.js
+++ app/server/components/repositories/repos.js
@@ -4,8 +4,6 @@ var github = require('../services/github');
 
 module.exports = {
 
-    client: github.client,
-
     getRepos: () => {
         return github.getRepos({
             org: github.config.org,

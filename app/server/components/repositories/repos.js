9a38954fe5248e217bc15f24ce97de8502724e94
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-06 11:06:26 -0800

    #19 - adding repo file

---
+++ app/server/components/repositories/repos.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var github = require('../services/github');
+
+module.exports = {
+    getRepos () {
+        return github.getRepos({
+            org: github.config.org,
+            per_page: 100
+        });
+    }
+};

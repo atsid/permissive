66a99260e2850d8689ce4f53e2b09bf68a3435a0
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-09 12:53:23 -0700

    #20 - getting repo permissions for each user

--- app/server/components/repositories/repos.js
+++ app/server/components/repositories/repos.js
@@ -10,5 +10,29 @@ module.exports = {
             org: github.config.org,
             per_page: 100
         });
+    },
+
+    getTeams () {
+        return github.getTeams({
+            org: github.config.org,
+            per_page: 100
+        });
+    },
+
+    getTeamMembers (teamId) {
+        return github.getTeamMembers({
+            id: teamId,
+            org: github.config.org,
+            per_page: 100
+        });
+    },
+
+    getTeamRepos (teamId) {
+        return github.getTeamRepos({
+            id: teamId,
+            org: github.config.org,
+            per_page: 100
+        });
     }
+
 };

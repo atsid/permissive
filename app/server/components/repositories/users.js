2bff599b01d91402bbf40e4cf248bf8ed3420da5
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-09 15:52:44 -0700

    #20 - moving logic from middleware to repository

--- app/server/components/repositories/users.js
+++ app/server/components/repositories/users.js
@@ -2,19 +2,158 @@
 
 var svcPath = '../services/github',
     mock = process.env.SERVICE === 'mock' ? '.mock' : '',
-    github = require(svcPath + mock);
+    github = require(svcPath + mock),
+    Bluebird = require('bluebird'),
+    permissionLevels = {
+        admin: 3,
+        push: 2,
+        pull: 1,
+        none: 0
+    };
+
+function convertGithubUser(user) {
+    return {
+        username: user.login,
+        name: user.name,
+        avatar_url: user.avatar_url
+    };
+}
+
+function getGithubUser(username) {
+    return github.getUser({
+        'user': username
+    }).then((user) => {
+        return convertGithubUser(user);
+    });
+}
+
+function getGithubUsers() {
+    return github.getUsers({
+        org: github.config.org,
+        per_page: 100
+    });
+}
+
+function getGithubTeams() {
+    return github.getTeams({
+        org: github.config.org,
+        per_page: 100
+    });
+}
+
+function getGithubTeamMembers(teamId) {
+    return github.getTeamMembers({
+        id: teamId,
+        org: github.config.org,
+        per_page: 100
+    });
+}
+
+function getGithubTeamRepos(teamId) {
+    return github.getTeamRepos({
+        id: teamId,
+        org: github.config.org,
+        per_page: 100
+    });
+}
+
+function isPermissiveManaged(repo) {
+    let name = repo.name,
+        prefix = 'zzz_permissive_repo_' + name + '_';
+
+    return (name.indexOf(prefix) === 0);
+}
+
+function getDefaultPermissions() {
+    return { github: 'none', permissive: 'none'};
+}
+
+function getPermissionMap() {
+
+    let repositoryMap = {};
+    return getGithubTeams().then((teams) => {
+
+        let rosters = [];
+        teams.forEach((team) => {
+
+            rosters.push(getGithubTeamMembers(team.id).then((roster) => {
+
+                let teamPermission = team.permission;
+                return getGithubTeamRepos(team.id).then((repos) => {
+
+                    repos.forEach((repo) => {
+
+                        let repoId = repo.id,
+                            permissiveManaged = isPermissiveManaged(repo),
+                            userMap = repositoryMap[repoId];
+
+                        if (!userMap) {
+                            userMap = repositoryMap[repoId] = {};
+                        }
+
+                        roster.forEach((member) => {
+
+                            let username = member.login,
+                                permission = userMap[username],
+                                current;
+
+                            if (!permission) {
+                                permission = userMap[username] = getDefaultPermissions();
+                            }
+
+                            if (permissiveManaged) {
+                                current = permission.permissive;
+                                if (permissionLevels[teamPermission] > permissionLevels[current]) {
+                                    permission.permissive = teamPermission;
+                                }
+                            } else {
+                                current = permission.github;
+                                if (permissionLevels[teamPermission] > permissionLevels[current]) {
+                                    permission.github = teamPermission;
+                                }
+                            }
+                        });
+                    });
+                });
+            }));
+        });
+
+        return Bluebird.all(rosters).then(() => {
+            return repositoryMap;
+        });
+    });
+}
+
 
 module.exports = {
+
     getUsers () {
-        return github.getUsers({
-            org: github.config.org,
-            per_page: 100
+        return getGithubUsers().then((users) => {
+            let profiles = [];
+            users.forEach((user) => {
+                profiles.push(getGithubUser(user.login).then((profile) => {
+                    user.name = profile.name;
+                }));
+            });
+            return Bluebird.all(profiles).then(() => {
+                return users.map((user) => {
+                    return convertGithubUser(user);
+                });
+            });
         });
     },
 
     getUser (username) {
-        return github.getUser({
-            user: username
+        return getGithubUser(username);
+    },
+
+    getPermissions (repoId, users) {
+        return getPermissionMap().then((map) => {
+            let userMap = map[repoId];
+            users.forEach((user) => {
+                user.permission = userMap[user.username] || getDefaultPermissions();
+            });
+            return users;
         });
     }
 };
--- app/server/middleware/users.js
+++ app/server/middleware/users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var userService = require('../components/repositories/users'),
+var userRepository = require('../components/repositories/users'),
     repoService = require('../components/repositories/repos'),
     Bluebird = require('bluebird');
 
@@ -10,29 +10,9 @@ module.exports = {
         console.log('listing users [' + req.path + ']');
         console.log('query:' + JSON.stringify(req.query, null, 2));
 
-        userService.getUsers().then((users) => {
-            let profiles = [];
-
-            users.forEach((user) => {
-                profiles.push(userService.getUser(user.login).then((profile) => {
-                    user.name = profile.name;
-                }));
-            });
-
-            Bluebird.all(profiles).then(() => {
-                console.log('all user profiles retrieved');
-
-                req.entity = users.map((user) => {
-                    return {
-                        username: user.login,
-                        name: user.name,
-                        avatar_url: user.avatar_url
-                    };
-                });
-
-                next();
-            });
-
+        userRepository.getUsers().then((users) => {
+            req.entity = users;
+            next();
         }).catch((err) => {
             next(err);
         });
@@ -45,89 +25,13 @@ module.exports = {
             users = req.entity;
 
         if (repoId) {
-
-            let repositoryMap = {};
-
-            // get teams
-            repoService.getTeams().then((teams) => {
-
-                console.log('got the teams');
-
-                let rosters = [];
-
-                // get team members
-                teams.forEach((team) => {
-
-                    rosters.push(repoService.getTeamMembers(team.id).then((roster) => {
-
-                        console.log('got team members:', team.name);
-
-                        let teamPermission = team.permission;
-
-                        return repoService.getTeamRepos(team.id).then((repos) => {
-
-                            repos.forEach((repo) => {
-
-                                console.log('got repo:', repo.name);
-
-                                let repoId = repo.id,
-                                    repoName = repo.name,
-                                    permissiveManaged = (repoName.indexOf('zzz_permissive_repo_' + repoName + '_') === 0),
-                                    userMap = repositoryMap[repoId];
-
-                                if (!userMap) {
-                                    userMap = repositoryMap[repoId] = {};
-                                }
-
-                                roster.forEach((member) => {
-
-                                    let username = member.login,
-                                        permission = userMap[username],
-                                        current,
-                                        permissionLevels = {
-                                            admin: 3,
-                                            push: 2,
-                                            pull: 1,
-                                            none: 0
-                                        };
-
-                                    if (!permission) {
-                                        permission = repositoryMap[repoId][username] = { permissive: 'none', github: 'none' };
-                                    }
-
-                                    if (permissiveManaged) {
-                                        current = permission.permissive;
-                                        if (permissionLevels[teamPermission] > permissionLevels[current]) {
-                                            repositoryMap[repoId][username].permissive = teamPermission;
-                                        }
-                                    } else {
-                                        current = permission.github;
-                                        if (permissionLevels[teamPermission] > permissionLevels[current]) {
-                                            repositoryMap[repoId][username].github = teamPermission;
-                                        }
-                                    }
-                                });
-                            });
-                        });
-                    }));
-                });
-
-                Bluebird.all(rosters).then(() => {
-                    console.log('got all the team members');
-                    let userMap = repositoryMap[repoId];
-                    users.forEach((user) => {
-                        user.permission = userMap[user.username] || { permissive: 'none', github: 'none' };
-                    });
-
-                    next();
-                });
-
+            userRepository.getPermissions(repoId, users).then((users) => {
+                req.entity = users;
+                next();
             }).catch((err) => {
                 next(err);
             });
         }
-
-        next();
     },
 
     listUsersLinks (req, res, next) {
@@ -157,15 +61,10 @@ module.exports = {
         console.log('getting user [' + req.path + ']');
         console.log('params:' + JSON.stringify(req.params, null, 2));
 
-        userService.getUser(req.params.username).then((profile) => {
-            req.entity = {
-                username: profile.login,
-                name: profile.name,
-                avatar_url: profile.avatar_url
-            };
-
+        let username = req.params.username;
+        userRepository.getUser(username).then((user) => {
+            req.entity = user;
             next();
-
         }).catch((err) => {
             next(err);
         });

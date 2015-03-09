9ad67250b467fb2753d8b421891350ca7919114a
natoverse <github@talkswithnumbers.com>
2015-03-09 13:24:27 -0700

    #23 - added object masking
    Masking uses the ‘fields’ paradigm to remove properties from returned
    objects for more realistic simulation of GitHub API.

--- app/server/components/services/github.mock.js
+++ app/server/components/services/github.mock.js
@@ -5,25 +5,26 @@
  * Methods map from raw GitHub API methods (as exposed through node-github) to our business methods.
  * The components/repositories bubble that up to higher-level business logic and domain models.
  */
-var users = [{
-    login: 'testuser1',
-    name: 'Test User 1',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/25254?v=3&s=400'
-}, {
-    login: 'testuser2',
-    name: 'Test User 2',
-    avatar_url: 'https://avatars1.githubusercontent.com/u/2741?v=3&s=400'
-}],
+var mask = require('json-mask'),
+    users = [{
+        login: 'testuser1',
+        name: 'Test User 1',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/25254?v=3&s=400'
+    }, {
+        login: 'testuser2',
+        name: 'Test User 2',
+        avatar_url: 'https://avatars1.githubusercontent.com/u/2741?v=3&s=400'
+    }],
     repos = [{
-    id: 1,
-    name: 'Test Repo 1',
-    description: 'A library for managing file io.',
-    private: false
-}, {
-    id: 2,
-    name: 'Test Repo 2',
-    private: true
-}];
+        id: 1,
+        name: 'Test Repo 1',
+        description: 'A library for managing file io.',
+        private: false
+    }, {
+        id: 2,
+        name: 'Test Repo 2',
+        private: true
+    }];
 
 module.exports = {
 
@@ -34,7 +35,9 @@ module.exports = {
     getUsers () {
         console.log('looking up mock users');
         return new Promise((resolve, reject) => {
-            resolve(users);
+            //getting the users as a list from github does not include the name field
+            let masked = users.map((user) => mask(user, 'login,avatar_url'));
+            resolve(masked);
         });
     },
 

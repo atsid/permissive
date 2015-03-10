89e5e71535fe50a76dc24d3ce1d063ec499d72c4
Brian Mathews <spoonart@gmail.com>
2015-03-09 19:21:00 -0700

    #21 - Adds concating of bower styles

--- app/client/gulp/tasks/styles.js
+++ app/client/gulp/tasks/styles.js
@@ -7,12 +7,17 @@ var csso = require('gulp-csso');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var es = require('event-stream');
 
 module.exports = gulp.task('styles', function () {
-  return gulp.src(config.paths.src.styles)
-    .pipe(gulpif(!release, sourcemaps.init()))
-    .pipe(sass())
-    .pipe(gulpif(!release, sourcemaps.write()))
+  return es.merge(
+        gulp.src(config.paths.src.styles)
+            .pipe(gulpif(!release, sourcemaps.init()))
+            .pipe(sass())
+            .pipe(gulpif(!release, sourcemaps.write())),
+        gulp.src(config.paths.src.bowerStyles))
+    .pipe(concat('app.css'))
     .pipe(autoprefixer('last 1 version'))
     .pipe(gulpif(release, csso()))
     .pipe(gulpif(release, rename(config.filenames.release.styles), rename(config.filenames.build.styles)))

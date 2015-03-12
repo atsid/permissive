13c520bfd1744df914b589a9189d103720283825
Nathan Evans <github@talkswithnumbers.com>
2015-03-12 16:53:45 -0700

    #78 - removed dead user permission resource method

--- app/client/src/modules/common/services/reposService.js
+++ app/client/src/modules/common/services/reposService.js
@@ -3,11 +3,6 @@
 module.exports = /*@ngInject*/
     function reposService($resource) {
         return $resource('api/v1/repos', {}, {
-            'query': {
-                method: 'GET',
-                isArray: true
-            },
-            //TODO: dealing with hypermedia would probably be best done directly with $http
-            'editUserPermission': { method: 'PUT', url: 'api/v1/:link'}
+            'query': { method: 'GET', isArray: true }
         });
     };

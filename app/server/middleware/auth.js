f932f8bc9abfd615947863be56d346b632ab5aa8
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-13 14:39:44 -0700

    (#103) - fixes in auth middleware

--- app/server/middleware/auth.js
+++ app/server/middleware/auth.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var user = { username: process.env.GITHUB_USERNAME };
+var username = process.env.GITHUB_USERNAME;
 
 module.exports = {
 
     authenticate (req, res, next) {
-        req.auth.username = user.username;
+        req.auth = { username: username };
+        next();
     }
 
 };

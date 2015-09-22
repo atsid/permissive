37405da7bd1f0c8698f5b6e947b9d339a3b5d7fa
David Tittsworth <stopyoukid@gmail.com>
2015-09-22 13:07:56 -0700

    A little more build shuffling

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-npm install -g gulp
-npm install bower
-
-export PATH=node_modules/bower/bin/:$PATH
-bower update # make sure packages are up to date.
-
 npm test
 
 npm install --production
@@ -15,4 +9,4 @@ cp -R ./node_modules ./target
 cp -R ./app ./target/app
 
 cd ./target
-zip -r ../permissive.zip .
+zip -q -r ../permissive.zip .
